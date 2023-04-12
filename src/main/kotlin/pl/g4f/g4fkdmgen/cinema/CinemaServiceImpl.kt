ebf72ae367340709730b0a3140104b9afd748836
kozio <koziolmail@gmail.com>
2023-04-12 13:34:24 +0200

    #2 Cinema list - cinemas are get from directory configured in properties

---
+++ src/main/kotlin/pl/g4f/g4fkdmgen/cinema/CinemaServiceImpl.kt
@@ -0,0 +1,20 @@
+package pl.g4f.g4fkdmgen.cinema
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.stereotype.Service
+import java.nio.file.Files
+import java.nio.file.Path
+import kotlin.io.path.isDirectory
+import kotlin.io.path.name
+
+@Service
+@ConfigurationProperties("g4f.cinema")
+class CinemaServiceImpl {
+    lateinit var path: Path
+    fun cinemas(name: String?): List<CinemaDto> =
+        Files.list(path)
+            .filter { it.isDirectory() }
+            .filter { if (name != null) it.name.contains(name) else true }
+            .map { CinemaDto(it.fileName.name) }
+            .toList()
+}
